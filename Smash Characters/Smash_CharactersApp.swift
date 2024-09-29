272ea67bd7fd34f0fa22622fc77b87e8ee545408
LennyRBriones <lennyrbriones@outlook.com>
2024-09-29 10:37:53 -0600

    Initial Commit

---
+++ Smash Characters/Smash_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smash_CharactersApp.swift
+//  Smash Characters
+//
+//  Created by Lenny R. Briones on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Smash_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smash CharactersTests/Smash_CharactersTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smash_CharactersTests.swift
+//  Smash CharactersTests
+//
+//  Created by Lenny R. Briones on 29/09/24.
+//
+
+import Testing
+@testable import Smash_Characters
+
+struct Smash_CharactersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
